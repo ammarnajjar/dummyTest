707e03623ab88caebe26d9f38cab9bdf65d5000e
Ammar Najjar <najjarammar@gmail.com>
2017-02-07 12:48:48 +0100

    Initial commit

---
+++ src/test/java/dummyTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Ammar Najjar
+ *
+ */
+public class dummyTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
